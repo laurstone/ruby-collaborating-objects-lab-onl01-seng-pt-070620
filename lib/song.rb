6a1241d3488fdd75b0fb78a31896057e5eaf6051
Lauren Stone <lstone321@gmail.com>
2020-08-10 22:26:09 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -16,7 +16,7 @@ class Song
   def self.new_by_filename(filename)
     song_pieces = filename.split(" - ")
      binding.pry 
-    new_song = self.new(songs)
+    new_song = self.new()
     new_song.artist_name = artist
   end
   
