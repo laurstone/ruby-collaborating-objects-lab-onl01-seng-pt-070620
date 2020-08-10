a2003dabe26c0de1669737eb9efc58f4abad2408
Lauren Stone <lstone321@gmail.com>
2020-08-10 22:12:30 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,39 @@
+require "pry"
+
+class Song
+  attr_accessor :name, :artist, :songs
+
+  @@all = []
+  
+
+  def initialize(name)
+    @name = name
+    @songs = []
+    @@all << self 
+
+  end
+# binding.pry 
+  def self.new_by_filename(filename)
+    @@all = filename.split(" - ")
+    new_songs = self.new(songs)
+    new_songs.artist_name = artist
+  end
+  
+  
+  
+  def self.find_by_artist(artist)
+    Song.all.select do |song|
+      song.artist == artist
+    end
+  end
+
+  def self.all
+    @@all
+  end
+
+  def artist_name=(name)
+    self.artist = Artist.find_or_create_by_name(name)
+  end
+
+
+end
